d95603420608fa9b0ba73a23f8e8d8fd5e82f9b8
starchasm_nyx <92284144+nox-katena@users.noreply.github.com>
2024-07-09 01:27:25 +0900

    Create seonah.h

---
+++ seonah.h
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+ 
+#define PRECISION 0
+ 
+#define fr first
+#define sc second
+ 
+using ll = long long;
+using ld = long double;
+ 
+typedef pair<ll,ll> pll;
+typedef pair<int,int> pii;
+ 
+const ll MOD = 1'000'000'007;
+const ll INF = 4'500'000'000'000'000'000;
+
+#define all(v) v.begin(), v.end()
+#define compress(v) sort(all(v)), v.erase(unique(all(v)), v.end())
